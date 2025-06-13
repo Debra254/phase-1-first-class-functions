d999ca396aa916b8985369a602ff25f6e3589697
Debra <debrawangeci@gmail.com>
2025-06-14 02:46:15 +0300

    Define receivesAFunction, returnsANamedFunction, and returnsAnAnonymousFunction

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+
+function receivesAFunction(callback) {
+  callback();
+}
+
+function returnsANamedFunction() {
+  return function namedFunction() {
+    return "Hello from named function";
+  };
+}
+
+function returnsAnAnonymousFunction() {
+  return function () {
+    return "Hello from anonymous function";
+  };
+}
